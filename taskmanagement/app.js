4ce1bfd202fbfa0e28658858a372a4bf628c930a
BirendraRai1 <birendrabit123@gmail.com>
2017-12-02 11:03:31 +0530

    second commited

--- taskmanagement/app.js
+++ taskmanagement/app.js
@@ -14,10 +14,10 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost/taskManagement");
 mongoose.connection.once('open',function(){
-  console.log("Database Connection Established Successfully.");
+	console.log("Database Connection Established Successfully.");
 });
 mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+	console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   //process.exit(1);
 });
 require('./app/models/taskAssign.js');
@@ -37,8 +37,8 @@ route.controllerFunction(app);
 
 //error handling middleware
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(err ||500).send('Something broke!')
+	console.error(err.stack)
+	res.status(err ||500).send('Something broke!')
 });
 
 app.listen(3000,()=>console.log("server running on port 3000"));
--- taskmanagement/middleWares/utils.js
+++ taskmanagement/middleWares/utils.js
@@ -1,11 +1,11 @@
 exports.generateRandomNumber=function(req,res,next){
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 8; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+	for (var i = 0; i < 8; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  req.suffix_id=text;
-  next();
+	req.suffix_id=text;
+	next();
 };
 
